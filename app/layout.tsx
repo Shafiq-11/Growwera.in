d7276b444ea3d2fd5f6b7352b865bd0bd8a76a68
Shafiq-11 <shafiqahamedsha11@gmail.com>
2026-09-17 15:39:16 +0530

    feat: remove left scroll tracking rail sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Plus_Jakarta_Sans, Instrument_Serif, Caveat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import ScrollTracker from "@/components/layout/ScrollTracker";
 import GrowweraLoader from "@/components/loading/GrowweraLoader";
 import PaperBackground from "@/components/decorative/PaperBackground";
 import ScrollProgress from "@/components/shared/ScrollProgress";
@@ -112,7 +111,6 @@ export default function RootLayout({
           <GrowweraLoader />
           <PaperBackground />
           <Navbar />
-          <ScrollTracker />
           <main className="flex-1 relative z-10">{children}</main>
           <Footer />
         </ThemeProvider>
